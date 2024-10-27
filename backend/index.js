ea5afc3fdec7fd8af274ed19146e2931f1b2b805
Mohammad-Irshad <mohammadirshadcse@gmail.com>
2024-10-27 15:59:56 +0530

    updated port

--- backend/index.js
+++ backend/index.js
@@ -70,7 +70,7 @@ app.patch("/book/update/:id", async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
